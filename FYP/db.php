e5233add80562dfc570001eb60aea570e3ce040a
Wanyee1405 <106374829+Wanyee1405@users.noreply.github.com>
2022-05-27 16:32:42 +0800

    Add files via upload

---
+++ FYP/db.php
@@ -0,0 +1,9 @@
+<?php
+$username = "root";
+$password = "";
+$dbname = "feedback";
+
+$conn = new PDO('mysql:host=localhost;dbname=' . $dbname . ';charset-utf8mb4', $username, $password);
+// set the PDO error mode to exception
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+?>
