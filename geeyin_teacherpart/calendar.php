e8cdb1e75c6a9b712b409c142ef8d86c2e8c4077
Gee Yin <106010108+ggy0321@users.noreply.github.com>
2022-05-30 18:37:12 +0800

    Add files via upload

---
+++ geeyin_teacherpart/calendar.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Check Calendar</title>
+</head>
+
+<body>
+    <h1>
+        Check Calendar
+    </h1>
+
+    <ul>
+    <li><a href="timetable.php">View Timetable</a></li>
+    <li><a href="viewannouncement.php">View or Add Announcement</a></li>
+    </ul>
+</body>
+</html>
