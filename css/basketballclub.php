55a8e9477fcd6369043c601aebc2edc365cf9507
SzeDong <105801160+SzeDong@users.noreply.github.com>
2022-06-02 16:44:33 +0800

    Add files via upload
    For Progress meeting check

---
+++ css/basketballclub.php
@@ -0,0 +1,63 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+        <link rel="stylesheet" href="css/club.css">
+        <title>Basketball Club</title>
+    </head>
+<body>
+    
+    <div class ="otherclubname">
+        <ul>
+            <li><a href="#"><i class="fa fa-house"></i>Home</a></li>
+            <li><a href="badmintonclub.php"><i class="fa fa-shuttlecock"></i>Badminton Club</a></li>
+            <li class="active"><a href="basketballclub.php"><i class="fa fa-basketball"></i>Basketball Club</a></li>
+            <li><a href="dancingclub.php"><i class="fa fa-music"></i>Dancing Club</a></li>
+            <li><a href="mediaclub.php"><i class="fa fa-camera"></i>Media Club</a></li>
+            <li><a href="orchestraclub.php"><i class="fa fa-drum"></i>Orchestra Club</a></li>
+            <li><a href="studentcouncilclub.php"><i class="fa fa-user-tie"></i>Student Council Club</a></li>
+        </ul>
+    </div>
+        <header>
+            <div class="header-content">
+                <h1>Welcome to Basketball Club! Come and join us!</h1>
+                <div class="line"></div>
+                <a href="#" class="ctn">Learn More</a>
+            </div>
+        </header>
+        <section>
+            <div class="title">
+                <h1>Description</h1>
+                <div class="line"></div>
+            </div>
+        </section>
+        <p>Our club provides a fun, and competitive badminton environment. The mission of our club is to educate and empower the members who love Basketball. Members will learn how to dribble, shoot, pass and new abilities to further their knowledge of basketball.
+            Feel free to join us if interested!</p>
+        </section>
+        <section>
+            <div class="title">
+                <h1>Upcoming Events</h1>
+                <div class="line"></div>
+            </div>
+        </section>
+        <section class="events">
+            <div class="title">
+                <h1>Badminton Club and Hierachy Photoes</h1>
+                <div class="line"></div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <img src="./css/basketballphoto.jpg" alt="">
+                    <h4>Basketball Game</h4>
+                    <p>A group of spirited passionate players playing basketball</p>
+                </div>
+                <div class="col">
+                    <img src="./css/basketballclub_hierarchy.jpg" alt="">
+                    <h4>The group hierarchy</h4>
+                    <p>Members who are tasked to mange the club well.</p>
+                </div>
+            </div>
+        </section>
+</body>
+</html>
