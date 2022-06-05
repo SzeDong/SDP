8b073447cb7669cf51c99ab3bddaadfeae34ad8b
Gee Yin <106010108+ggy0321@users.noreply.github.com>
2022-06-05 16:05:04 +0800

    Delete geeyin_teacherpart directory

--- geeyin_teacherpart/connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-$con=mysqli_connect("localhost","root", "" ,"sdp_assignment");
-
-if (mysqli_connect_errno())
-    {
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
-    }
-?>
